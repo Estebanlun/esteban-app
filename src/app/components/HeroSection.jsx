5289f3b3c2f999104e3a0de4d08599f4d1e1c960
Esteban Luna Claraso <estebanlunacl@gmail.com>
2024-02-20 11:02:46 -0300

    endProyect

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,87 @@
+"use client";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const link = "https://drive.google.com/file/d/1-DBdVNic66sM5fZFig7n-jax4fkDwi9X/view?usp=sharing"
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-orange-400 ">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Esteban Luna",
+                2000,
+                "Full-Stack",
+                2000,
+                "Web Developer",
+                2000,
+                "Front-end Developer",
+                2000,
+                "Back-end Developer",
+                2000,
+                "Coach",
+                2000,
+              ]}
+              wrapper="span"
+              speed={10}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7DBE] text-base sm:text-sm sm:w-60 lg:w-auto mb-6 lg:text-xl">
+            I enjoy writing code, playing sports, playing video games, spending
+            time with my loved ones and I drink a lot of mate 🧉🇦🇷.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 to-blue-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href={link}
+              target="_blank"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-orange-500 to-blue-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="Hero-Image"
+              width={500}
+              height={500}
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/app/components/ProjectSection.jsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "React Portfolio WebSite",
+    description: "React website",
+    image: "/images/projects/1.png",
+    gitUrl: "/",
+    tag: ["All", "Web", "Mobile"],
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Dev-Sky",
+    description:
+      "SPA (Single Page Application) on sale of real flights. Developed with ReactJS on the Front-end, with NodeJS and Express on the Back-end and with a non-SQL Firebase database. In it, I mainly worked on the Front-end. We use Sass and other tools to style it. This project helped me a lot to learn to work as a team. It enriched my communication, and helped me reaffirm my technical knowledge.",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Countries Project",
+    description:
+      "SPA (Single Page Application) about countries. Developed with ReactJS, Redux on the Front-end, with NodeJS, Express and Sequalize on the Back-end. And for the database use PostgresSQL. This project was a real challenge, I spent hours and hours looking for solutions to all the problems that arose. I did it alone, something that gave me confidence and allowed me to consolidate my technical knowledge and everything I learned during my course.",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "NextJs Portfolio WebSite",
+    description:
+      "Updated portfolio page, made with NextJs, Tailwind and deployed with Firebase.",
+    image: "/images/projects/4.png",
+    tag: ["All", "Web", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+const ProjectSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectSection;
